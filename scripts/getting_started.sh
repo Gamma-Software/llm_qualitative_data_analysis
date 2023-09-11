bd8ffb7fc920fd810198342f22495a45e270aad7
Valenti Rudloff <valentin.rudloff.perso@gmail.com>
2023-09-11 21:32:26 +0200

    updated getting started

--- scripts/getting_started.sh
+++ scripts/getting_started.sh
@@ -1,2 +1,9 @@
 virtualenv .venv
 .venv/bin/python -m pip install -r requirements.txt
+
+if [ ! -f ./.streamlit/secrets.toml ]; then
+    cp ./.streamlit/secrets_template.toml ./.streamlit/secrets.toml
+    read -p  "Please enter your openai api key: " replace
+    sed -i "s/your key here/$replace/" ./.streamlit/secrets.toml
+fi
+.venv/bin/python -m streamlit run qualitative_analyse_agent.py
